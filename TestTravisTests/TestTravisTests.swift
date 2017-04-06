ba76ea4cafbf61b541e5eb5fcd6658235eb5a136
Mert Senyurt <mert.senyurt@home24.de>
2017-04-06 10:49:17 +0200

    Added unit failing unit test. Added simulator to travis config.

--- TestTravisTests/TestTravisTests.swift
+++ TestTravisTests/TestTravisTests.swift
@@ -32,5 +32,9 @@ class TestTravisTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+
+    func testFailUnit() {
+        XCTAssertTrue(false)
+    }
     
 }
